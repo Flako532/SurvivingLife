a26fd58edacd02ca16f6871abec490b18b6a4003
Flako532 <felipeadelgadog@gmail.com>
2019-10-24 04:11:32 -0300

    Added GameMode

---
+++ gameMode.py
@@ -0,0 +1,14 @@
+"""
+This module controls the game and has all attributes refered to the game.
+
+Lorem Ipsum.
+"""
+import pygame
+
+
+class GameMode():
+    """Class manages game rules and flow."""
+
+    def __init__(self):
+        """Docstring."""
+        pass
---
+++ main.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+"""
+This is an example script.
+
+It seems that it has to have THIS docstring with a summary line, a blank line
+and sume more text like here. Wow.
+"""
+import pygame
+import sys
+pygame.init()
+
+size = width, height = 1080, 720
+speed = [2, 2]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.Surface((70, 70))
+pygame.draw.circle(ball, pygame.Color(125, 125, 125), (35, 35), 35)
+ballrect = ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
