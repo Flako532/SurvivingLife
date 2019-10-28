29151b87c7b01ca41352fb92da21766018489f69
Flako532 <felipeadelgadog@gmail.com>
2019-10-28 14:04:14 -0300

    Added Controller

---
+++ controller.py
@@ -0,0 +1,14 @@
+"""
+Docs.
+
+Docs
+"""
+
+
+class ClassName(object):
+    """docstring for ClassName."""
+
+    def __init__(self, arg):
+        """Docs."""
+        super(ClassName, self).__init__()
+        self.arg = arg
